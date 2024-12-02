17b20551d9683218f01496bd97816f0a68f123f1
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-12-02 16:47:45 +0100

    Deployment Fix 5

--- src/app/dashboard/listings/page.jsx
+++ src/app/dashboard/listings/page.jsx
@@ -1,13 +1,14 @@
 "use client";
 
+import { useTheme } from "@/app/context/themeContext";
 import { appState } from "@/appState"; // Application state to access the username
 import { MenuItems } from "@/data"; // Mock data for calls
 import dynamic from "next/dynamic"; // For dynamic imports
 
 // Dynamically import components with SSR disabled
 const Humphry = dynamic(() => import("@/Components/Humphry"), { ssr: false });
-const Header = dynamic(() => import("../../Components/dashboard/Header"), { ssr: false });
-const SideBar = dynamic(() => import("../../Components/dashboard/SideBar"), { ssr: false });
+const Header = dynamic(() => import("@/Components/dashboard/Header"), { ssr: false });
+const SideBar = dynamic(() => import("@/Components/dashboard/SideBar"), { ssr: false });
 const MainContent = dynamic(() => import("./MainContent"), { ssr: false });
 
 const page = () => {
