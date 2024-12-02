c1dfe38bbcd3241947f2459cbd6b309129cc72ab
JoJo <89042174+JoJoDevAdventure@users.noreply.github.com>
2024-12-02 16:45:24 +0100

    Deployment Fix 4

--- src/app/dashboard/listings/page.jsx
+++ src/app/dashboard/listings/page.jsx
@@ -1,12 +1,14 @@
 "use client";
 
-import { useTheme } from "@/app/context/themeContext"; // Theme context for dark/light mode
 import { appState } from "@/appState"; // Application state to access the username
-import Humphry from "@/Components/Humphry";
 import { MenuItems } from "@/data"; // Mock data for calls
-import Header from "../../../Components/dashboard/Header"; // Header component
-import SideBar from "../../../Components/dashboard/SideBar"; // Sidebar navigation component
-import MainContent from "./MainContent"; // Main content component
+import dynamic from "next/dynamic"; // For dynamic imports
+
+// Dynamically import components with SSR disabled
+const Humphry = dynamic(() => import("@/Components/Humphry"), { ssr: false });
+const Header = dynamic(() => import("../../Components/dashboard/Header"), { ssr: false });
+const SideBar = dynamic(() => import("../../Components/dashboard/SideBar"), { ssr: false });
+const MainContent = dynamic(() => import("./MainContent"), { ssr: false });
 
 const page = () => {
   const { isDarkMode } = useTheme(); // Get the current theme (dark or light mode)
